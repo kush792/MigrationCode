22b96dc90892e3f3c83f9043742c9ff308778c07
Satender <kushagr.bajpai@algoworks.com>
2021-09-23 10:25:32 +0000

    Added user's company

---
+++ src/main/java/com/gitMigration/gitMigration/AddUser.java
@@ -0,0 +1,12 @@
+package com.gitMigration.gitMigration;
+
+public class AddUser {
+	String name;
+	int number;
+	String userCompName;
+	
+	void getUser(String name, int number, String userCompName){
+		System.out.println("User Name : "+name+" User Number : "+number+" User Company : "+userCompName);
+	}
+
+}
