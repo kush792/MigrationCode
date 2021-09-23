f9611f51090bb1878ed435b7e7ebb77295e3869d
Anurag_grg <kushagrbajpai1@gmail.com>
2021-09-23 10:31:37 +0000

    added get user method

--- src/main/java/com/gitMigration/gitMigration/AddUser.java
+++ src/main/java/com/gitMigration/gitMigration/AddUser.java
@@ -8,5 +8,13 @@ public class AddUser {
 	void getUser(String name, int number, String userCompName){
 		System.out.println("User Name : "+name+" User Number : "+number+" User Company : "+userCompName);
 	}
+	
+	String getUserByNumber(int number) {
+		String userName = "";
+		if(number == 01) {
+			userName = "Anurag";
+		}
+		return userName;
+	}
 
 }
